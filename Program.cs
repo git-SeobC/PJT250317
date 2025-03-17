59672ffb7d7ad950c3ad11986eb9d21dc0f6aa93
SBC <seobc789@gmail.com>
2025-03-17 15:46:13 +0900

    Json example Update

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq.Expressions;
+
 namespace PJT250317
 {
     internal class Program
     {
-        class HelloWorld
+        class GameObject
         {
-            public HelloWorld(int inGold = 100, int inHP = 100, int inMP = 100)
+            public GameObject(int inGold = 100, int inHP = 100, int inMP = 100)
             {
                 Gold = inGold;
                 MP = inMP;
-                HP = inGold;
+                HP = inHP;
             }
 
             public int Gold;
@@ -17,15 +21,22 @@ namespace PJT250317
         }
         static void Main(string[] args)
         {
-            HelloWorld h = new HelloWorld(10, 20, 30);
+            List<GameObject> gameObjects = new List<GameObject>();
+            gameObjects.Add(new GameObject(1, 2, 3));
+            gameObjects.Add(new GameObject(4, 5, 6));
+            gameObjects.Add(new GameObject(7, 8, 9));
+            gameObjects.Add(new GameObject(10, 11, 12));
 
-            StreamReader sr = new StreamReader("/data.dat");
-            string DataGold = sr.ReadLine();
-            string DataHP = sr.ReadLine();
-            string DataMP = sr.ReadLine();
+            string jsonData = JsonConvert.SerializeObject(gameObjects); // 객체를 Json 데이터로 변환
 
-            sr.Close();
+            Console.WriteLine(jsonData);
 
+            List<GameObject> g2 = JsonConvert.DeserializeObject<List<GameObject>>(jsonData); // Json 데이터를 객체로 변환
+
+            foreach(var go in g2)
+            {
+                Console.WriteLine(go.Gold);
+            }
         }
     }
 }
